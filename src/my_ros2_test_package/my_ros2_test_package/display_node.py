c47463d8b04663530bc26c296b6ff013c36a24ac
fidelyspiritus <anastasiia.a.markova@gmail.com>
2024-01-08 16:11:40 +0300

    display node

---
+++ src/my_ros2_test_package/my_ros2_test_package/display_node.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+
+class DisplaNode(Node):
+    def __init__(self):
+        super.__init__('display_node')
+        self.temperature_subscription = self.create_subscription(
+            Float64, 
+            'temperature', 
+            self.temperature_callback, 
+            10)
+        self.light_subscription = self.create_subscription(
+            Float64,
+            'light',
+            self.light_callback,
+            10)
+        self.temperature_subscription   #to prevent unused variable warning
+        self.light_subscription
+
+    def temperature_callback(self, msg):
+        self.get_logger().info('Temperatures: "%s" Celsius' % msg.data)
+    
+    def light_callback(self, msg):
+        self.get_logger().info('Light level: "%s" Lux' % msg.data)
+
+def main(args = None):
+    rclpy.init(args = args)
+    display_node = DisplaNode()
+    rclpy.spin(display_node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
