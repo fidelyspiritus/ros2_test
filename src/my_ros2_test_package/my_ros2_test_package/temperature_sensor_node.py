b763d1dd99b36f5bd1ba8e1aea333899862659b3
fidelyspiritus <anastasiia.a.markova@gmail.com>
2024-01-08 14:24:09 +0300

    create pkg, temperature sensor

---
+++ src/my_ros2_test_package/my_ros2_test_package/temperature_sensor_node.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+from random import uniform
+import time
+
+class TemperatureSensorNode(Node):
+    def __init__(self):
+        super().__init__('temperature_sensor')
+        self.publisher_ = self.create_publisher(Float64, 'temperature', 10)
+        self.timer_period = 1.0 #seconds
+        self.timer = self.create_timer(self.timer_period, self.publish_temperature)
+
+    def publish_temperature(self):
+        temperature = uniform(15.0, 25.0)
+        msg = Float64()
+        msg.data = temperature
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+def main(args = None):
+    rclpy.init(args = args)
+    temperature_sensor_node = TemperatureSensorNode()
+    rclpy.spin(temperature_sensor_node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/my_ros2_test_package/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_ros2_test_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='anastasiia',
+    maintainer_email='anastasiia@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'temperature_sensor = my_sor2_test_package.temperature_sensor_node:main',
+            'light_sensor = my_sor2_test_package.light_sensor_node:main',
+            'display = my_sor2_test_package.display_node:main',
+        ],
+    },
+)
