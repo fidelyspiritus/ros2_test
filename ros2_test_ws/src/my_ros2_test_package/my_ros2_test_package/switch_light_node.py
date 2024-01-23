8fcd3a3da8bda0fcb9f41497bb0de5f7763f3f3f
Anastasiia <79062589+fidelyspiritus@users.noreply.github.com>
2024-01-23 15:51:42 +0300

    delete publisher

--- ros2_test_ws/src/my_ros2_test_package/my_ros2_test_package/switch_light_node.py
+++ ros2_test_ws/src/my_ros2_test_package/my_ros2_test_package/switch_light_node.py
@@ -1,35 +1,25 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64, String
+from std_msgs.msg import Float64
 
 
 class SwitchLightNode(Node):
     def __init__(self):
-        super().__init__('switch_light')
-        self.light_level = None
+        super().__init__('switch_light') 
         self.light_level_subscription = self.create_subscription(
             Float64,
             'lightness',
             self.light_level_callback,
             10)
         self.light_level_subscription
-        self.publisher_ = self.create_publisher(String, 'switch_light', 10)
-        self.timer_period = 5.0 #seconds
-        self.timer = self.create_timer(self.timer_period, self.publish_light_switch_status)
+        self.light_threshold = 500.0 # Уровень освещенности для включения света
 
-    def publish_light_switch_status(self):
-        if self.light_level is not None:
-            if self.light_level > 500.0:
-                status = "Turn off the light, it's bright enought!"
+    def light_level_callback(self, msg):
+        if msg.data is not None:
+            if msg.data > self.light_threshold:
+                self.get_logger().info("Turn off the light, it's bright enough!")
             else:
-                status = "Turn on the light, it's too dark outside!"
-        msg = String()
-        msg.data = status
-        self.publisher_.publish(msg)
-        self.get_logger().info(status)
-    
-    def light_level_callback(self,msg):
-        self.light_level = msg.data
+                self.get_logger().info("Turn on the light, it's too dark outside!") 
 
 def main(args = None):
     rclpy.init(args = args)
